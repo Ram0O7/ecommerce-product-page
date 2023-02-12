8bb315b7b50876067196aeaaa2e5021f43327290
Ramkrishn <ram706860@gmail.com>
2023-02-12 20:52:37 +0530

    last commit

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,11 +7,11 @@ const NavBar = ({ showCart, cartCount }) => {
   const [showSidebar, setShowSidebar] = useState(false);
 
   const navList = <ul className={classes['nav-list']}>
-    <a href='/'><li className={classes['list-item']}>collections</li></a>
-    <a href='/'><li className={classes['list-item']}>men</li></a>
-    <a href='/'><li className={classes['list-item']}>women</li></a>
-    <a href='/'><li className={classes['list-item']}>about</li></a>
-    <a href='/'><li className={classes['list-item']}>contact</li></a>
+    <a href='/ecommerce-product-page'><li className={classes['list-item']}>collections</li></a>
+    <a href='/ecommerce-product-page'><li className={classes['list-item']}>men</li></a>
+    <a href='/ecommerce-product-page'><li className={classes['list-item']}>women</li></a>
+    <a href='/ecommerce-product-page'><li className={classes['list-item']}>about</li></a>
+    <a href='/ecommerce-product-page'><li className={classes['list-item']}>contact</li></a>
   </ul>
 
   return (
@@ -20,7 +20,7 @@ const NavBar = ({ showCart, cartCount }) => {
         <div className={classes.menu} onClick={() => setShowSidebar(!showSidebar)}>
           <svg width="16" height="15" xmlns="http://www.w3.org/2000/svg"><path d="M16 12v3H0v-3h16Zm0-6v3H0V6h16Zm0-6v3H0V0h16Z" fill="#69707D" fillRule="evenodd" /></svg>
         </div>
-        <a href='/'><h1>sneakers</h1></a>
+        <a href='/ecommerce-product-page'><h1>sneakers</h1></a>
       </div>
       {showSidebar &&
         <React.Fragment>
@@ -30,11 +30,11 @@ const NavBar = ({ showCart, cartCount }) => {
               <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg"><path d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z" fill="#f45" fillRule="evenodd" /></svg>
             </div>
             <ul className={classes['sidebar-list']}>
-              <a href='/'><li className={classes['list-item']}>collections</li></a>
-              <a href='/'><li className={classes['list-item']}>men</li></a>
-              <a href='/'><li className={classes['list-item']}>women</li></a>
-              <a href='/'><li className={classes['list-item']}>about</li></a>
-              <a href='/'><li className={classes['list-item']}>contact</li></a>
+              <a href='/ecommerce-product-page'><li className={classes['list-item']}>collections</li></a>
+              <a href='/ecommerce-product-page'><li className={classes['list-item']}>men</li></a>
+              <a href='/ecommerce-product-page'><li className={classes['list-item']}>women</li></a>
+              <a href='/ecommerce-product-page'><li className={classes['list-item']}>about</li></a>
+              <a href='/ecommerce-product-page'><li className={classes['list-item']}>contact</li></a>
             </ul>
           </div>
         </React.Fragment>
